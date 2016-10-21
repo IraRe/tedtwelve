fe007730f879a64ad1cf9c25890705d37989cf5b
DennisCreutz <dennis.creutz@prodyna.com>
2016-10-21 10:14:20 +0200

    Database values are now parsed into a table.

--- src/main/docker/apache/resources/app/main.js
+++ src/main/docker/apache/resources/app/main.js
@@ -1,20 +1,58 @@
 /**
  * Created by dcreutz on 20.10.2016.
  */
-$(function () {
-    $("#submitButton").click(function () {
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MainController', ['$scope', function ($scope) {
+
+    $scope.onSubmitClick = function () {
         var params = {
             name: $("#nameInput").val()
         };
 
         $.ajax({
             type: "POST",
-            url: "http://192.168.99.100:8080/user",
+            url: getUrl() + restUrls.addUser,
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(params),
             success: function (e) {
-                console.log("It worked! Id: "+e.id);
+                console.log("It worked! Id: " + e.id);
+                console.log("Refresh usernames.");
+
+                getUsernames();
+            },
+            error: function (request, status, error) {
+                console.error("Error: " + request.responseText);
             }
         });
-    });
-});
+    };
+
+    var REST_PORT = "8080";
+    var restUrls = {
+        addUser: '/user',
+        showUser: '/username',
+        helloworld: '/helloworld'
+    };
+
+    function getUrl() {
+        return window.location.protocol + "//" + window.location.hostname + ":" + REST_PORT;
+    }
+
+    function getUsernames() {
+        $.ajax({
+            type: "GET",
+            url: getUrl() + restUrls.showUser,
+            contentType: "application/json; charset=utf-8",
+            success: function (e) {
+                console.log("It worked! Data: " + e);
+                $scope.userNames = e;
+            },
+            error: function (request, status, error) {
+                console.error("Error: " + request.responseText);
+            }
+        });
+    }
+
+    $scope.userNames = getUsernames();
+
+}]);
